22ed0551f8132b3acc3cd36e9b88d63b522a6f56
blooddot <blooddot@163.com>
2016-12-15 20:55:28 +0800

    FF Rush

---
+++ FF Rush/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour
+{
+    private Rigidbody2D _rigidbody2D;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    this._rigidbody2D = this.gameObject.GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+
+    void FixedUpdate()
+    {
+        this._rigidbody2D.AddForce(Vector2.right * 10);
+
+    }
+}
