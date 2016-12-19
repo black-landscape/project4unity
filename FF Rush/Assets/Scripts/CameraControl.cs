52ffabc03125fa4956ab7e495e728c8aa28b872b
blooddot <blooddot@163.com>
2016-12-19 17:11:29 +0800

    FF Rush

---
+++ FF Rush/Assets/Scripts/CameraControl.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraControl : MonoBehaviour
+{
+    public float xSmooth = 8f;      // How smoothly the camera catches up with it's target movement in the x axis.
+    public float ySmooth = 8f;		// How smoothly the camera catches up with it's target movement in the y axis.
+    private Transform player;       // Reference to the player's transform.
+
+    // Use this for initialization
+    void Start ()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+           
+	}
+
+    void FixedUpdate()
+    {
+        TrackPlayer();
+    }
+
+
+    void TrackPlayer()
+    {
+        // By default the target x and y coordinates of the camera are it's current x and y coordinates.
+        float targetX = transform.position.x;
+        float targetY = transform.position.y;
+
+        // If the player has moved beyond the x margin...
+        // ... the target x coordinate should be a Lerp between the camera's current x position and the player's current x position.
+        //targetX = Mathf.Lerp(transform.position.x, player.position.x, xSmooth * Time.deltaTime);
+        targetX = player.position.x;
+
+        // If the player has moved beyond the y margin...
+        // ... the target y coordinate should be a Lerp between the camera's current y position and the player's current y position.
+        targetY = Mathf.Lerp(transform.position.y, player.position.y, ySmooth * Time.deltaTime);
+
+        // Set the camera's position to the target position with the same z component.
+        //transform.position = new Vector3(targetX, targetY, transform.position.z);
+
+        transform.position = new Vector3(targetX, targetY, transform.position.z);
+    }
+}
--- FF Rush/Assets/Scripts/PlayerControl.cs
+++ FF Rush/Assets/Scripts/PlayerControl.cs
@@ -4,22 +4,70 @@ using System.Collections;
 public class PlayerControl : MonoBehaviour
 {
     private Rigidbody2D _rigidbody2D;
+    private Transform _transform;
+
+    private float _oy = 0.0f;
+    private float _cy;
+    private float _moveGapY = 1.2f;
+    private float _sgravity = 2.0f;
+    private float _egravity = 6.0f;
+    private float _forceY = 5000.0f;
+
+    private bool _isJumping = false;
 
 	// Use this for initialization
 	void Start ()
 	{
+        this._transform = this.gameObject.GetComponent<Transform>();
 	    this._rigidbody2D = this.gameObject.GetComponent<Rigidbody2D>();
+        this._rigidbody2D.AddForce(Vector2.right * 2000);
 
+        
     }
 
     // Update is called once per frame
-    void Update () {
-	
-	}
+    void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //this._rigidbody2D.MovePosition(this._rigidbody2D.position + Vector2.up);
+
+            this._isJumping = true;
+            this._rigidbody2D.AddForce(Vector2.up * this._forceY);
+            this._rigidbody2D.gravityScale = this._sgravity;
+        }
+
+        if (this._isJumping)
+        {
+            this._cy = this._transform.position.y;
+            if (0 != this._oy)
+            {
+                Debug.Log("this._oy------" + this._oy);
+                Debug.Log("this._cy - this._oy------" + (this._cy - this._oy));
+                if (this._cy - this._oy >= this._moveGapY)
+                {
+                    this._isJumping = false;
+                    this._oy = 0;
+                    this._rigidbody2D.gravityScale = this._egravity;
+                }
+                else
+                {
+                    //this._rigidbody2D.MovePosition(this._rigidbody2D.position + Vector2.up * this._moveY);
+
+                }
+            }
+            else
+            {
+                Debug.Log("this._oy------null");
+                //this._rigidbody2D.MovePosition(this._rigidbody2D.position + Vector2.up * this._moveY);
+                this._oy = this._transform.position.y;
+            }
+        }
+    }
 
     void FixedUpdate()
     {
-        this._rigidbody2D.AddForce(Vector2.right * 10);
-
+        //this._rigidbody2D.AddForce(Vector2.right * 10);
+        //this._rigidbody2D.MovePosition(this._rigidbody2D.position + Vector2.right);
     }
 }
