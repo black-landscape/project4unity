f9ceb517ad1fba211e1c3c22a95a904abb8d68e1
blooddot <blooddot@163.com>
2017-01-10 00:36:18 +0800

    FF Rush 单例 对象池

---
+++ FF Rush/Assets/Resources/Scripts/PoolManager.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoolManager : MonoBehaviour
+{
+    public GameObject beamPrefab;
+
+    private Dictionary<PoolEnum, List<GameObject>> _poolDict;
+
+    private bool _isInit;
+
+    void Start()
+    {
+        
+    }
+
+    public void pushToPool(PoolEnum key, GameObject obj)
+    {
+        this.checkInit();
+
+        if (this._poolDict.ContainsKey(key))
+        {
+            this._poolDict[key].Add(obj);
+        }
+        else
+        {
+            this._poolDict.Add(key, new List<GameObject>());
+        }
+    }
+
+    public GameObject pullFromPool(PoolEnum key)
+    {
+        this.checkInit();
+
+        GameObject obj;
+        obj = Instantiate(beamPrefab) as GameObject;
+
+        if (this._poolDict.ContainsKey(key) && this._poolDict[key].Count > 0)
+        {
+            obj = this._poolDict[key][0];
+            this._poolDict[key].RemoveAt(0);
+        }
+        else
+        {
+            switch (key)
+            {
+                case PoolEnum.BEAM:
+                    obj = Instantiate(beamPrefab) as GameObject;
+                    break;
+                default:
+                    obj = null;
+                    break;
+            }
+        }
+
+        return obj;
+    }
+
+    private void checkInit()
+    {
+        if (!this._isInit)
+        {
+            this._isInit = true;
+            this._poolDict = new Dictionary<PoolEnum, List<GameObject>>();
+
+            beamPrefab = Resources.Load("Prefabs/beam_prefab", typeof(GameObject)) as GameObject;
+        }
+    }
+}
+
+public enum PoolEnum{
+    BEAM
+}
--- FF Rush/Assets/Resources/Scripts/RulerControl.cs
+++ FF Rush/Assets/Resources/Scripts/RulerControl.cs
@@ -4,34 +4,35 @@ using UnityEngine;
 
 public class RulerControl : MonoBehaviour
 {
-	private GameObject[] _lines;
-
-	public GameObject line_prefab;
+	private List<GameObject> _lines;
 
 	void Start()
 	{
-		this._lines = new GameObject[0];
+        this._lines = new List<GameObject>();
 
+        PoolManager pm = Singleton.getInstance("PoolManager") as PoolManager;
 		for (int i = 0; i < 10; i++) {
-			GameObject line = GameObject.Instantiate (line_prefab);
+            GameObject line = pm.pullFromPool(PoolEnum.BEAM);
 			LineRenderer lineRenderer = line.GetComponent<LineRenderer> ();
 
 			lineRenderer.numPositions = 2;
-
 			lineRenderer.SetPosition (0, new Vector3 (0,i*0.6f,0));
 			lineRenderer.SetPosition (1, new Vector3 (1000,i*0.6f,0));
-		}
+
+            this._lines.Add(line);
+        }
 
 		for (int j = 0; j < 10; j++) {
-			GameObject line = GameObject.Instantiate (line_prefab);
-			LineRenderer lineRenderer = line.GetComponent<LineRenderer> ();
+            GameObject line = pm.pullFromPool(PoolEnum.BEAM);
+            LineRenderer lineRenderer = line.GetComponent<LineRenderer> ();
 
 			lineRenderer.numPositions = 2;
-
 			lineRenderer.SetPosition (0, new Vector3 (j*0.6f,0,0));
 			lineRenderer.SetPosition (1, new Vector3 (j*0.6f,1000,0));
-		}
-	}
+
+            this._lines.Add(line);
+        }
+    }
 
 	void Update()
 	{
