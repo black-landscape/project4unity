7be8cb6b03bc501b899925b0dfb43b644c70a790
blooddot <blooddot@163.com>
2017-02-07 18:12:49 +0800

    Snake

---
+++ Snake/Assets/Scripts/Snake.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Snake : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+	{
+	    InvokeRepeating("Move", 0.3f, 0.3f);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+
+    void Move()
+    {
+        
+    }
+}
---
+++ Snake/Assets/Scripts/SpawnFood.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnFood : MonoBehaviour
+{
+    public GameObject foodPrefab;
+
+    public Transform borderTop;
+    public Transform borderBottom;
+    public Transform borderLeft;
+    public Transform borderRight;
+
+	// Use this for initialization
+	void Start ()
+    {
+	    InvokeRepeating("Spawn", 3, 4);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+
+    void Spawn()
+    {
+        int x = (int) Random.Range(borderLeft.position.x, borderRight.position.x);
+        int y = (int) Random.Range(borderBottom.position.y, borderTop.position.y);
+
+        Instantiate(foodPrefab, new Vector2(x, y), Quaternion.identity);
+    }
+}
