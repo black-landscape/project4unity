308f5ed35ce226c5221f4b9b34e6785f7e95fe7f
blooddot <blooddot@163.com>
2017-02-08 18:07:06 +0800

    Pong

---
+++ Pong/Assets/Scripts/Ball.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour
+{
+    public float speed = 30;
+
+    // Use this for initialization
+    void Start()
+    {
+        GetComponent<Rigidbody2D>().velocity = Vector2.right * speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "RacketLeft")
+        {
+            float y = hitFactor(transform.position, collision.transform.position, collision.collider.bounds.size.y);
+            Vector2 dir = new Vector2(1, y).normalized;
+            GetComponent<Rigidbody2D>().velocity = dir*speed;
+        }
+
+        if (collision.gameObject.name == "RacketRight")
+        {
+            float y = hitFactor(transform.position, collision.transform.position, collision.collider.bounds.size.y);
+            Vector2 dir = new Vector2(-1,y).normalized;
+
+            GetComponent<Rigidbody2D>().velocity = dir*speed;
+        }
+    }
+
+    float hitFactor(Vector2 ballPos, Vector2 racketPos, float racketHeight)
+    {
+        return (ballPos.y - racketPos.y)/racketHeight;
+    }
+}
