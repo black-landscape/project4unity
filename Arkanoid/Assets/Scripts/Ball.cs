cc0770717ce440c469f7cdc56e3ac59e49a97516
blooddot <blooddot@163.com>
2017-02-09 15:11:52 +0800

    Arkanoid

---
+++ Arkanoid/Assets/Scripts/Ball.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour
+{
+    public float speed = 100.0f;
+
+    // Use this for initialization
+    void Start()
+    {
+        GetComponent<Rigidbody2D>().velocity = Vector2.up * speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "racket")
+        {
+            float x = hitFactor(transform.position, collision.transform.position, collision.collider.bounds.size.x);
+            Vector2 dir = new Vector2(x,1).normalized;
+            GetComponent<Rigidbody2D>().velocity = dir*speed;
+        }
+    }
+
+    float hitFactor(Vector2 ballPos, Vector2 racketPos, float racketWidth)
+    {
+        return (ballPos.x - racketPos.x)/racketWidth;
+    }
+}
---
+++ Arkanoid/Assets/Scripts/Racket.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Racket : MonoBehaviour
+{
+    public float speed = 150;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void FixedUpdate()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+
+        GetComponent<Rigidbody2D>().velocity = Vector2.right*h*speed;
+    }
+}
+
